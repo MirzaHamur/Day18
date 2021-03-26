593ff6a0a8194478926bfdfb639fb679a022f08c
Mirza <mirza.hamurko@gmail.com>
2021-03-26 01:14:15 +0100

    ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+let arena = new Map();
+arena.set("author", "Gigi");//Author
+arena.set("publisher", "Mistral");//Publisher
+arena.set("numberOfPages",28);//Pages
+arena.set("year", 2021); //Year
+// Only change code above this line
+for(let value of arena) console.log(value);
+module.exports = arena;
